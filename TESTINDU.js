5d92f897a631a1f6adefe50d717396eb8f804211
Indunil1992 <indunil@adroitlogic.com>
2020-04-09 11:44:22 +0530

    1

--- TESTINDU.js
+++ TESTINDU.js
@@ -1,4 +1,18 @@
+let AWS = require('aws-sdk');
+const ses = new AWS.SES();
+const s3 = new AWS.S3();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed 1"};
+    try {
+        let data = await s3.listObjects({
+            Bucket: "as2-test-lahiru",
+            MaxKeys: 10
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+        
+
+    return { "message": "Successfully executed 1" };
 };
